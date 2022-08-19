5dc9c0365177e07c69673e0e6e1245e75f450fb7
Ingo Ruhnke <grumbel@gmail.com>
2022-08-19 17:35:34 +0200

    Initial checkin

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+
+    tinycmmc.url = "github:grumbel/tinycmmc";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+
+    libvorbis_src.url = "https://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.xz";
+    libvorbis_src.flake = false;
+
+    libogg.url = "github:grumnix/libogg-win32";
+  };
+
+  outputs = { self, nixpkgs, tinycmmc, libvorbis_src, libogg }:
+    tinycmmc.lib.eachWin32SystemWithPkgs (pkgs:
+      {
+        packages = rec {
+          default = libvorbis;
+
+          libvorbis = pkgs.stdenv.mkDerivation {
+            pname = "libvorbis";
+            version = "1.3.7";
+
+            src = libvorbis_src;
+
+            buildInputs = [
+              libogg.packages.${pkgs.system}.default
+            ];
+          };
+        };
+      }
+    );
+}
