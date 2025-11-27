d698e14ec3fce00f85c111f74ad783bbda813a72
Ingo Ruhnke <grumbel@gmail.com>
2025-11-27 14:58:04 +0100

    Update to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
@@ -26,7 +26,7 @@
             src = libvorbis_src;
 
             buildInputs = [
-              libogg.packages.${pkgs.system}.default
+              libogg.packages.${pkgs.stdenv.hostPlatform.system}.default
             ];
           };
         };
